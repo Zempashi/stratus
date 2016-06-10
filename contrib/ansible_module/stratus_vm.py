d5d37e2bd5fbde31cbe2b8ee0c91b3d3ebcb677d
Julien Girardin <jugirardin@free.fr>
2016-06-10 21:06:45 +0200

    Ansible module start working !

--- contrib/ansible_module/stratus_vm.py
+++ contrib/ansible_module/stratus_vm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import sys
 import time
 import operator
@@ -10,12 +10,13 @@ except ImportError:
     HAS_ARGPARSE = False
 
 try:
-    import urllib.parse as urlparse
+    from urllib.parse import urljoin
 except ImportError:
-    import urlparse
+    from urlparse import urljoin
 
 USER_AGENT = "ansible-stratus-vm/0.0.1"
 
+#############################
 # some code of the six module
 PY2 = sys.version_info[0] == 2
 PY3 = sys.version_info[0] == 3
@@ -57,6 +58,8 @@ _add_doc(itervalues, "Return an iterator over the values of a dictionary.")
 _add_doc(iteritems,
          "Return an iterator over the (key, value) pairs of a dictionary.")
 
+#############################
+
 
 class StratusVMAnsible(object):
 
@@ -66,11 +69,13 @@ class StratusVMAnsible(object):
 
     def execute(self, http_agent, vm_list, api_endpoint, state, **kwargs):
         self.api_root = self.normalize_url(api_endpoint)
-        vm_dict = create_vm_dict(vm_list)
+        vm_dict = self.create_vm_dict(vm_list)
         self.find_api_root()
         vm_found = self.existing_vm(vm_dict)
-        vm_processed = self.action_vm()
+        vm_processed = self.action_vm(vm_dict, vm_found, state)
         changed = bool(vm_processed)
+        self.poll_result(vm_processed, state)
+        return {'changed': changed}
 
     def create_vm_dict(self, vm_list):
         parser = argparse.ArgumentParser()
@@ -90,13 +95,13 @@ class StratusVMAnsible(object):
 
     def existing_vm(self, vm_dict):
         vm_found = dict()
-        # vms_endpoint = urlparse.join(self.api_root, 'vms')
-        for vm_name in vm_dict.iterkeys():
-            vm_url = urlparse.join(self.api_root, 'vms', vm_name)
+        # vms_endpoint = urljoin(self.api_root + '/', 'vms')
+        for vm_name in iterkeys(vm_dict):
+            vm_url = urljoin(self.api_root + '/', 'vms/' + vm_name)
             resp, status = fetch_url(self.module, vm_url)
             if status['status'] == 200:
                 try:
-                    found_vm[vm] = json.loads(resp.read())
+                    vm_found[vm_name] = json.loads(resp.read().decode('utf-8'))
                 except ValueError:
                     continue
         return vm_found
@@ -106,52 +111,55 @@ class StratusVMAnsible(object):
         if state == 'absent':
             vm_action = viewkeys(vm_dict) & viewkeys(vm_found)
 
-            def action(vm): self.delete_vm(vm)
+            def action(vm): return self.delete_vm(vm)
         elif state == 'present':
             vm_action = viewkeys(vm_dict) - viewkeys(vm_found)
 
-            def action(vm): self.create_vm(vm_dict[vm])
+            def action(vm): return self.create_vm(vm_dict[vm])
         for vm in vm_action:
             vm_processed[vm] = action(vm) or vm_found[vm]
         return vm_processed
 
-    def delete_vm(vm_id=None, vm_name=None):
+    def delete_vm(self, vm_id=None, vm_name=None):
         if not vm_id and not vm_name:
             raise ValueError('Please provide a \'vm_id\' or \'vm_name\'')
-        vm_url = urlparse.join(self.api_root, 'vms', vm_id or vm_name)
+        vm_url = urljoin(self.api_root + '/', 'vms/' + vm_id or vm_name)
         resp, status = fetch_url(self.module, vm_url, method='DELETE')
         if status['status'] not in [200, 204]:
             raise ValueError('Error deleting VM \'%s\'' % (vm_id or vm_name))
 
-    def create_vm(vm_spec):
+    def create_vm(self, vm_spec):
         if vm_spec is not None:
-            vm_data = json.dumps(dict(args=vm_spec))
+            vm_data = json.dumps(dict(args=vm_spec)).encode('utf-8')
         else:
-            raise StratusVMAnsibleError('You need to provide complete'
+            raise StratusVMAnsibleError('You need to provide complete '
                                         'install line for creating vm')
-        post_vm_url = urlparse.join(self.api_root, 'vms')
-        resp, status = fetch_url(self.module, post_vm_url, data=vm_data)
-        if status['status'] in [200, 204]:
+        post_vm_url = urljoin(self.api_root + '/', 'vms')
+        resp, status = fetch_url(self.module, post_vm_url, data=vm_data,
+                                 headers={'Content-Type': 'application/json'},
+                                 method='POST')
+        if status['status'] in [200, 201]:
             try:
-                return json.loads(resp.read())
+                temp = json.loads(resp.read().decode('utf-8'))
+                return temp
             except ValueError:
-                pass
-        raise ValueError('Error creating VM \'%s\'' % (vm_id or vm_name))
+                raise
+        raise ValueError('Error creating VM \'%s\'' % (vm_spec))
 
-    def poll_result(vm_processed, state):
-        max_error = 10
+    def poll_result(self, vm_processed, state):
         if state == 'absent':
             target_status = ['DELETED']
         elif state == 'present':
             target_status = ['STARTED', 'STOPPED']
+        max_error = 10
         while max_error > 0:
             i = -1
-            for i, (vm_name, vm_info) in enumerate(viewitems(vm_processed)):
-                vm_url = urlparse.join(self.api_root, 'vms', vm_info['id'])
-                resp, status = fetch_url(self.module, api_root)
+            for i, (vm_name, vm_info) in enumerate(list(iteritems(vm_processed))):
+                vm_url = urljoin(self.api_root + '/', 'vms/' + str(vm_info['id']))
+                resp, status = fetch_url(self.module, vm_url)
                 if status['status'] == 200:
                     try:
-                        j = json.loads(resp)
+                        j = json.loads(resp.read().decode('utf-8'))
                     except ValueError:
                         max_error -= 1
                         continue
@@ -167,28 +175,27 @@ class StratusVMAnsible(object):
 
     @staticmethod
     def normalize_url(url):
-        url_parts = urlparse.urlparse(url)
+        url_parts = urlparse(url)
         if not url_parts.netloc:
             url = 'http://{}'.format(url)
-            url_parts = urlparse.urlparse(url)
+            url_parts = urlparse(url)
         if not url_parts.path:
             url = '{}/'.format(url)
         return url
 
     def find_api_root(self):
         api_list = [lambda: self.api_root,
-                    lambda: urlparse.join(self.api_root, 'v1')]
+                    lambda: urljoin(self.api_root + '/', 'v1')]
         insert_count = 10  # Prevent infinite redirection loop
         for i, api in enumerate(api_list):
             api_root = api()
             resp, status = fetch_url(self.module, api_root)
             try:
-                j = json.loads(r.read())
-            except ValueError:
+                j = json.loads(resp.read().decode('utf-8'))
+            except (ValueError, AttributeError):
                 continue
             else:
-                if j.hasattr('vms') and j.hasattr('hkvms') and\
-                        status['status'] == 200:
+                if 'vms' in j and 'hkvms' in j and status['status'] == 200:
                     break
                 else:
                     try:
@@ -228,7 +235,7 @@ def main():
         module.fail_json(msg="Please install argparse")
     try:
         res_dict = m.execute(**module.params)
-    except StratusVMAnsibleError, exc:
+    except StratusVMAnsibleError as exc:
         module.fail_json(msg=exc.msg)
     except Exception:
         import traceback
