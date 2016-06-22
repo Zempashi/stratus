9f9347ef873de0d4be46904b2f3c2c87ca9fa12f
Julien Girardin <julien.girardin@arkena.com>
2016-06-22 19:51:31 +0200

    Add tests (but fails for now)

--- stratus/tests/test_requests.py
+++ stratus/tests/test_requests.py
@@ -10,17 +10,16 @@ class CreateVM(TestCase):
         """Try to create an already existing vm"""
 	c = Client()
         response = c.post('/v1/vms',
-	    {'hkvm': None,
-	     'args': '-n pipo.vm -m 4096 --disk :10248'})
+	    {'args': '-n pipo.vm -m 4096 --disk :10248'})
         self.assertEqual(response.status_code, 400)
-	self.assertEqual(len(VM.objects.filter(name='pipo.vm')), 1)
+	self.assertEqual(len(VM.objects.all()), 1)
 
     def test_create_no_conflict(self):
         """Try to create an already existing vm"""
 	c = Client()
         response = c.post('/v1/vms',
-	    {'hkvm': None,
-	     'args': '-n pipo2.vm -m 4096 --disk :10248'})
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(VM.objects.filter(name='pipo.vm')), 2)
+	    {'args': '-n pipo2.vm -m 4096 --disk :10248'})
+        print(response.content)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(len(VM.objects.all()), 2)
 
