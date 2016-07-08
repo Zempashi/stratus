e3dd56367265a72dc78441d92675a1840f46334e
Julien Girardin <julien.girardin@arkena.com>
2016-07-08 19:26:23 +0200

    Fix example file for hkvm_mapping

--- stratus_site/hkvm_mapping.py
+++ stratus_site/hkvm_mapping.py
@@ -2,6 +2,5 @@
 from stratus.allocators.hkvmallocator import Map, _all_
 
 mapping = [
-    Map('abusi.*\.pg-1\.arkena\.net', 'hkvmg1'),
-    Map('jgirardi.*\.pg-1\.arkena\.net', 'hkvmg2'),
+    Map('.*', _all_),
     ]
